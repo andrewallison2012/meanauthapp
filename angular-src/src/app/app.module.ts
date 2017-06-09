77fcfd59debc46a6af82d6ff55e84a2869e41695
andrewallison2012 <andrewallison2012@gmail.com>
2017-06-09 11:01:22 -0500

    good

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// we imported this manuly to get our routes
+import { RouterModule, Routes } from "@angular/router";
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,6 +13,23 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+// so we can imoirt appRoutes below in th emodule imports as an object 
+//type of routes the = an array
+// types are capitalilzed
+//list of paths
+const appRoutes: Routes = [
+  //home path, uses HomeComponent imported from above
+  {path:'', component: HomeComponent},
+  //register path, uses HomeComponent imported from above
+  {path:'register', component: RegisterComponent},
+  //login path, uses HomeComponent imported from above
+  {path:'login', component: LoginComponent},
+  //dashboard path, uses HomeComponent imported from above
+  {path:'dashboard', component: DashboardComponent},
+  //profile path, uses HomeComponent imported from above
+  {path:'profile', component: ProfileComponent}
+];
 
 @NgModule({
   //this is where u add your components
@@ -26,7 +46,8 @@ import { ProfileComponent } from './components/profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   //this is where you add your services
   providers: [],
