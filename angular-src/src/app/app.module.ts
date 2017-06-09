78d1b3c3f2d3ad894f058940e459ba354ed2da76
andrewallison2012 <andrewallison2012@gmail.com>
2017-06-09 10:43:09 -0500

    good

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,16 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
+  //this is where u add your components
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
+  //this is where you add all your modules u are using
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  //this is where you add your services
   providers: [],
   bootstrap: [AppComponent]
 })
